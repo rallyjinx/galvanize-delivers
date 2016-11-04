8a43378c2020eba852082e8fb214fbdda2789766
Rally Jinx <rsjinx@gmail.com>
2016-11-04 15:43:55 -0500

    the stuff works but it looks terrible

---
+++ app.js
@@ -0,0 +1,64 @@
+//when "add to order" button pushed
+//add item to order, calculate subtotal, tax, total
+var subtotal = 0;
+var tax = 0;
+var total = 0;
+var burgerQ = 0;
+var pizzaQ = 0;
+var icQ = 0;
+var ribQ = 0;
+var burgerP = 0;
+var pizzaP = 0;
+var icP = 0;
+var ribP = 0;
+function taxCalc (subtotal) {
+  return (subtotal * .08);
+}
+function totalCalc (tax) {
+  return (subtotal + tax);
+}
+function quantity(name, price) {
+  if (name === "burger") {
+    $('#burger-qty').text('');
+    $('#burger-prc').text('');
+    burgerQ += 1;
+    burgerP += parseFloat(price);
+    $('#burger-qty').text(burgerQ);
+    $('#burger-prc').text(burgerP.toFixed(2));
+  } else if (name === "pizza") {
+    $('#pizza-qty').text('');
+    $('#pizza-prc').text('');
+    pizzaQ += 1;
+    pizzaP += parseFloat(price);
+    $('#pizza-qty').text(pizzaQ);
+    $('#pizza-prc').text(pizzaP.toFixed(2));
+  } else if (name === "ice-cream") {
+    $('#ic-qty').text('');
+    $('#ic-prc').text('');
+    icQ += 1;
+    icP += parseFloat(price);
+    $('#ic-qty').text(icQ);
+    $('#ic-prc').text(icP.toFixed(2));
+  } else {
+    $('#rib-qty').text('');
+    $('#rib-prc').text('');
+    ribQ += 1;
+    ribP += parseFloat(price);
+    $('#rib-qty').text(ribQ);
+    $('#rib-prc').text(ribP.toFixed(2));
+  }
+}
+
+$('.order-button').on('click', function() {
+  //add button's value attritube to subtotal, tax, total
+  $('#subtotal').text('');
+  $('#tax').text('');
+  $('#total').text('');
+  subtotal += parseFloat(this.value);
+  $('#subtotal').append(subtotal.toFixed(2));
+  tax = taxCalc(parseFloat(subtotal));
+  $('#tax').append(tax.toFixed(2));
+  total = totalCalc(parseFloat(tax));
+  $('#total').append(total.toFixed(2  ));
+  quantity(this.id, this.value);
+});
